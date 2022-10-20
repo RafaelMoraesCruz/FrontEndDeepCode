1f036e03154c87fa1169e2f7a3f567861e9bda0c
RafaelMoraesCruz <rafaelgeorge344@gmail.com>
2022-10-20 19:28:25 -0300

    professors put implementation

--- Professors/professors.js
+++ Professors/professors.js
@@ -2,8 +2,13 @@ const table = document.getElementById("professor")
 const resultContainer =  document.getElementById('professorsList')
 const inputName = document.getElementById("input-name")
 const inputCPF = document.getElementById("input-cpf")
-const inputDepartmentId = document.getElementById("departmentId")
+const inputDepartmentId = document.getElementById("departmentIdC")
 const btnSalvar = document.getElementById("btn-create-professor")
+const inputNameUpdate = document.getElementById("input-name-update")
+const inputCpfUpdate = document.getElementById("input-cpf-update")
+const inputDepartmentUpdate = document.getElementById("departmentIdUpdate")
+
+let actualId = 0
 
 async function showAllProfessors(){
     const response = await fetch("http://localhost:8080/professors")
@@ -74,6 +79,38 @@ async function remover(id,name, row){
     }
 }
 
+async function updateProfessor(){
+    const name = inputNameUpdate.value.trim();
+    const cpf = inputCpfUpdate.value.trim();
+    const department = inputDepartmentUpdate.value
+    if (name){
+        // console.log(name)
+        // console.log(cpf)
+        // console.log(department)
+        // console.log(actualId)
+        const response = await fetch("http://localhost:8080/professors/"+actualId,
+        {method:"PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            cpf: cpf,
+            departmentId: department,
+            name: name
+        })},)
+    }
+    window.location.reload();
+}
+
+async function openUpdateModal(id,name,cpf,department){
+    actualId = id
+    inputNameUpdate.value = name
+    inputCpfUpdate.value = cpf
+    showAllDepartments()
+    // inputDepartmentUpdate.value = department.id
+    // inputDepartmentUpdate.textContent = department.name
+}
+
 async function removeAllProfessors(){
     const result = confirm("Are you sure, you want to REMOVE ALL professors?")
     if (result){
@@ -93,14 +130,11 @@ function createRow({id,name,cpf,department}){
     const departmentColumn = document.createElement("td")
     const acoesColumn = document.createElement("td")
 
-    const btnDelete = document.createElement("button")
-    btnDelete.classList.add("btn-info")
-    btnDelete.innerHTML = '<img src="../IMAGES/trash.svg"></img>';
+    btnDelete = createBtnDelete()
     btnDelete.addEventListener("click", () => remover(id,name,row))
 
-    const btnEdit = document.createElement("button")
-    btnEdit.classList.add("btn-info")
-    btnEdit.innerHTML = '<img src="../IMAGES/edit.svg"></img>';
+    btnEdit = createBtnEdit()
+    btnEdit.addEventListener("click", () => openUpdateModal(id,name,cpf,department))
 
     acoesColumn.setAttribute("class", "acoes-column")
     acoesColumn.appendChild(btnDelete)
@@ -124,16 +158,18 @@ function createRow({id,name,cpf,department}){
 
 async function showAllDepartments(){
     inputDepartmentId.innerHTML = ''
+    inputDepartmentUpdate.innerHTML= ''
     const response = await fetch("http://localhost:8080/departments")
     if(response.ok){
         const departments = await response.json();
         departments.forEach((department) => {
-            createDepartmentsSelection(department);
+            createDepartmentsSelectionCreateModal(department);
+            createDepartmentsSelectionUpdateModal(department);
         });
     }
 }
 
-function createDepartmentsSelection({id, name}){
+function createDepartmentsSelectionCreateModal({id, name}){
     const selection = document.createElement("OPTION")
     selection.setAttribute("value", id)
     selection.textContent = name
@@ -141,4 +177,28 @@ function createDepartmentsSelection({id, name}){
     inputDepartmentId.appendChild(selection)
 }
 
+function createDepartmentsSelectionUpdateModal({id, name}){
+    const selection = document.createElement("OPTION")
+    selection.setAttribute("value", id)
+    selection.textContent = name
+
+    inputDepartmentUpdate.appendChild(selection)
+}
+
 showAllProfessors()
+
+function createBtnEdit(){
+    const btnEdit = document.createElement("button")
+    btnEdit.setAttribute('data-bs-toggle',"modal")
+    btnEdit.setAttribute('data-bs-target',"#form-modal-update")
+    btnEdit.classList.add("btn-info")
+    btnEdit.innerHTML = '<img src="../IMAGES/edit.svg"></img>';
+    return btnEdit
+}
+
+function createBtnDelete(){
+    const btnDelete = document.createElement("button")
+    btnDelete.classList.add("btn-info")
+    btnDelete.innerHTML = '<img src="../IMAGES/trash.svg"></img>';
+    return btnDelete
+}
