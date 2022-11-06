61eab1c9f715eb9d607265d12f95e15b291c5d37
Ronaldo Edicassio de Siqueira <ronaldo.edicassio@gmail.com>
2022-11-06 13:47:59 -0300

    upadte professor - msg when name is empty

--- Professors/professors.js
+++ Professors/professors.js
@@ -1,5 +1,5 @@
 const table = document.getElementById("professor")
-const resultContainer =  document.getElementById('professorsList')
+const resultContainer = document.getElementById('professorsList')
 const inputName = document.getElementById("input-name")
 const inputCPF = document.getElementById("input-cpf")
 const inputDepartmentId = document.getElementById("departmentIdC")
@@ -10,11 +10,11 @@ const inputDepartmentUpdate = document.getElementById("departmentIdUpdate")
 
 let actualId = 0
 
-async function showAllProfessors(){
+async function showAllProfessors() {
     const response = await fetch("http://localhost:8080/professors")
-    if(response.ok){
+    if (response.ok) {
         const professors = await response.json();
-        if(professors.length > 0){
+        if (professors.length > 0) {
             table.removeAttribute("hidden")
         }
         professors.forEach((professor) => {
@@ -23,19 +23,19 @@ async function showAllProfessors(){
     }
 }
 
-document.addEventListener("keypress", function (tecla){
-    if(tecla.which == 13){
+document.addEventListener("keypress", function (tecla) {
+    if (tecla.which == 13) {
         searchProfessors()
     }
 })
 
-async function searchProfessors(){
+async function searchProfessors() {
     resultContainer.innerHTML = ''
     const professorNameForSearch = document.getElementById('professorNameForSearch')
-    const response = await fetch("http://localhost:8080/professors?name="+professorNameForSearch.value)
-    if(response.ok){
+    const response = await fetch("http://localhost:8080/professors?name=" + professorNameForSearch.value)
+    if (response.ok) {
         const professors = await response.json();
-        if(professors.length > 0){
+        if (professors.length > 0) {
             table.removeAttribute("hidden")
         }
         professors.forEach((professor) => {
@@ -44,77 +44,81 @@ async function searchProfessors(){
     }
 }
 
-async function addProfessor(){
+async function addProfessor() {
     var name = inputName.value.trim();
     name = verifyNameSize(name);
     const cpf = inputCPF.value.trim();
     const departmentId = inputDepartmentId.value.trim();
-    if (name){
-        const response = await fetch("http://localhost:8080/professors", 
-        {method:"POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            cpf: cpf,
-            departmentId: departmentId,
-            name: name
-        })},)
+    if (name) {
+        const response = await fetch("http://localhost:8080/professors",
+            {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    cpf: cpf,
+                    departmentId: departmentId,
+                    name: name
+                })
+            },)
     }
     window.location.reload();
 }
 
-async function remover(id,name, row){
-    const result = confirm("Você deseja remover o professor: " +name)
-    if (result){
-        const response = await fetch("http://localhost:8080/professors"+"/"+id, {method:"DELETE"})
-        if (response.ok){
+async function remover(id, name, row) {
+    const result = confirm("Você deseja remover o professor: " + name)
+    if (result) {
+        const response = await fetch("http://localhost:8080/professors" + "/" + id, { method: "DELETE" })
+        if (response.ok) {
             resultContainer.removeChild(row)
             window.location.reload();
         }
     }
 }
 
-async function updateProfessor(){
+async function updateProfessor() {
     var name = inputNameUpdate.value.trim();
     name = verifyNameSize(name);
     const cpf = inputCpfUpdate.value.trim();
     cpf
     const department = inputDepartmentUpdate.value
-    if (name){
-        const response = await fetch("http://localhost:8080/professors/"+actualId,
-        {method:"PUT",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            cpf: cpf,
-            departmentId: department,
-            name: name
-        })},)
+    if (name) {
+        const response = await fetch("http://localhost:8080/professors/" + actualId,
+            {
+                method: "PUT",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    cpf: cpf,
+                    departmentId: department,
+                    name: name
+                })
+            },)
     }
     window.location.reload();
 }
 
-async function openUpdateModal(id,name,cpf,department){
+async function openUpdateModal(id, name, cpf, department) {
     actualId = id
     inputNameUpdate.value = name
     inputCpfUpdate.value = cpf
     showAllDepartments(department)
 }
 
-async function removeAllProfessors(){
+async function removeAllProfessors() {
     const result = confirm("Are you sure, you want to REMOVE ALL professors?")
-    if (result){
-        const response = await fetch("http://localhost:8080/professors", {method:"DELETE"})
-        if (response.ok){
+    if (result) {
+        const response = await fetch("http://localhost:8080/professors", { method: "DELETE" })
+        if (response.ok) {
             resultContainer.innerHTML = ""
             window.location.reload();
         }
     }
 }
 
-function createRow({id,name,cpf,department}){
+function createRow({ id, name, cpf, department }) {
     const row = document.createElement("tr")
     const idColumn = document.createElement("td")
     const nameColumn = document.createElement("td")
@@ -123,17 +127,17 @@ function createRow({id,name,cpf,department}){
     const acoesColumn = document.createElement("td")
 
     btnDelete = createBtnDelete()
-    btnDelete.addEventListener("click", () => remover(id,name,row))
+    btnDelete.addEventListener("click", () => remover(id, name, row))
 
     btnEdit = createBtnEdit()
-    btnEdit.addEventListener("click", () => openUpdateModal(id,name,cpf,department))
+    btnEdit.addEventListener("click", () => openUpdateModal(id, name, cpf, department))
 
     acoesColumn.setAttribute("class", "acoes-column")
     acoesColumn.appendChild(btnDelete)
     acoesColumn.appendChild(btnEdit)
 
-    row.setAttribute("class","professor-row")
-    idColumn.setAttribute("scope","row")
+    row.setAttribute("class", "professor-row")
+    idColumn.setAttribute("scope", "row")
     idColumn.textContent = id
     nameColumn.textContent = name
     cpfColumn.textContent = cpf
@@ -148,22 +152,22 @@ function createRow({id,name,cpf,department}){
     resultContainer.appendChild(row)
 }
 
-async function showAllDepartments(selectedDepartment){
+async function showAllDepartments(selectedDepartment) {
     inputDepartmentId.innerHTML = ''
-    inputDepartmentUpdate.innerHTML= ''
+    inputDepartmentUpdate.innerHTML = ''
     const response = await fetch("http://localhost:8080/departments")
-    if(response.ok){
+    if (response.ok) {
         const departments = await response.json();
         departments.forEach((department) => {
             createDepartmentsSelectionCreateModal(department);
         })
         departments.forEach((department) => {
-            createDepartmentsSelectionUpdateModal(department,selectedDepartment);
+            createDepartmentsSelectionUpdateModal(department, selectedDepartment);
         })
     }
 }
 
-function createDepartmentsSelectionCreateModal({id, name}){
+function createDepartmentsSelectionCreateModal({ id, name }) {
     const selection = document.createElement("OPTION")
     selection.setAttribute("value", id)
     selection.textContent = name
@@ -171,12 +175,11 @@ function createDepartmentsSelectionCreateModal({id, name}){
     inputDepartmentId.appendChild(selection)
 }
 
-function createDepartmentsSelectionUpdateModal({id, name}, selectedDepartment){
+function createDepartmentsSelectionUpdateModal({ id, name }, selectedDepartment) {
     const selection = document.createElement("OPTION")
     selection.setAttribute("value", id)
     selection.textContent = name
-    if(id == selectedDepartment.id)
-    {
+    if (id == selectedDepartment.id) {
         selection.setAttribute("selected", true)
     }
     inputDepartmentUpdate.appendChild(selection)
@@ -184,45 +187,48 @@ function createDepartmentsSelectionUpdateModal({id, name}, selectedDepartment){
 
 showAllProfessors()
 
-function createBtnEdit(){
+function createBtnEdit() {
     const btnEdit = document.createElement("button")
-    btnEdit.setAttribute('data-bs-toggle',"modal")
-    btnEdit.setAttribute('data-bs-target',"#form-modal-update")
+    btnEdit.setAttribute('data-bs-toggle', "modal")
+    btnEdit.setAttribute('data-bs-target', "#form-modal-update")
     btnEdit.classList.add("btn-info")
     btnEdit.innerHTML = '<img src="../IMAGES/edit.svg"></img>';
     return btnEdit
 }
 
-function createBtnDelete(){
+function createBtnDelete() {
     const btnDelete = document.createElement("button")
     btnDelete.classList.add("btn-info")
     btnDelete.innerHTML = '<img src="../IMAGES/trash.svg"></img>';
     return btnDelete
 }
 
-function verifyNameSize(name){
+function verifyNameSize(name) {
     name = name.trim()
-    if(name.length <= 2) {
+    if (name.length == 0) {
+        alert("name is empty!")
+    } else if (name.length <= 2) {
         alert("name is too short, not saved")
-    } else{
+    }
+    else {
         return name
     }
 }
 
-function fMasc(objeto,mascara) {
-obj=objeto
-masc=mascara
-setTimeout("fMascEx()",1)
+function fMasc(objeto, mascara) {
+    obj = objeto
+    masc = mascara
+    setTimeout("fMascEx()", 1)
 }
 
 function fMascEx() {
-obj.value=masc(obj.value)
+    obj.value = masc(obj.value)
 }
 
-function mCPF(cpf){
-cpf=cpf.replace(/\D/g,"")
-cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
-return cpf
+function mCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
 }
